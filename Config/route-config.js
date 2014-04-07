1c27f3521b840fd7d79b9308bae7d0040f9c5081
Donniewiko <Jeroenmin@gmail.com>
2014-04-07 23:06:46 +0200

    version 1 of client

---
+++ Config/route-config.js
@@ -0,0 +1,17 @@
+﻿(function() {
+   'use strict';
+
+   var app = angular.module('app');
+
+   app.config(['$stateProvider', '$urlRouterProvider', routeConfigurator]);
+   function routeConfigurator($stateProvider, $urlRouterProvider) {
+      $urlRouterProvider.otherwise('/');
+      $stateProvider.state('login', {
+         templateUrl: "App/login.html",
+         controller: "loginController"
+      }).state('member', {
+         templateUrl: "App/member.html",
+         controller: "memberController"
+      });
+   }
+})();
