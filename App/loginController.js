831c9f43e55ad84f953ef0118f050a514b5e3177
Donniewiko <Jeroenmin@gmail.com>
2014-04-15 05:02:39 +0100

    modified style and functionality login

--- App/loginController.js
+++ App/loginController.js
@@ -6,50 +6,57 @@
       var signalRService;
       $scope.connectionID = '';
       //Init
-       function setSignalRSource() {
-         var s = document.createElement('script'); // use global document since Angular's $document is weak
-         s.src = 'http://' + $scope.signalRSource + '/signalr/hubs';
-         document.body.appendChild(s);
-         // Wait 0.5 seconds for the script tags to be added
-         setTimeout(function() {
-            $.connection.hub.url = 'http://' + $scope.signalRSource + '/signalr';
-            var serverHub = $.connection.serverHub;
+       var SignalRSource = {
+         Init: function(){
+            var s = document.createElement('script'); // use global document since Angular's $document is weak
+            s.src = 'http://' + $scope.signalRSource + '/signalr/hubs';
+            document.body.appendChild(s);
+            // Wait 0.5 seconds for the script tags to be added
+         },
 
-            // Init the client methods for the PlanningPoker.Server to call.
-            serverHub.client.registerUser = function (connectionID) {
-               $scope.connectionID = connectionID;
-            };
-
-            serverHub.client.proceedLogin = function (dashboard) {
-                  $state.go('member');
-            };
-            
-            serverHub.client.submitCards = function (pokerCards) {
-               $rootScope.$broadcast('pokerCards', pokerCards);
-            };
-
-            serverHub.client.sessionInProgress = function (inProgress) {
-               $rootScope.$broadcast('sessionInProgress', inProgress);
-            };
-
-            serverHub.client.showResults = function(results) {
-               console.log(results);
-               $rootScope.$broadcast('results', results);
-            };
-            serverHub.client.cardReceived = function() {
-               $rootScope.$broadcast('cardReceived');
-            };
-
-            $.connection.hub.start({ jsonp: true }).done(function() {
-               signalRService = $injector.get('signalRService');
+         Connect: function(){
+            $imeout(function() {
+               $.connection.hub.url = 'http://' + $scope.signalRSource + '/signalr';
+               var serverHub = $.connection.serverHub;
                
-            });
+               
+               // Init the client methods for the PlanningPoker.Server to call.
+               serverHub.client.registerUser = function (connectionID) {
+                  $scope.connectionID = connectionID;
+               };
+
+               serverHub.client.proceedLogin = function (dashboard) {
+                     $state.go('member');
+               };
+               
+               serverHub.client.submitCards = function (pokerCards) {
+                  $rootScope.$broadcast('pokerCards', pokerCards);
+               };
+
+               serverHub.client.sessionInProgress = function (inProgress) {
+                  $rootScope.$broadcast('sessionInProgress', inProgress);
+               };
+
+               serverHub.client.showResults = function(results) {
+                  console.log(results);
+                  $rootScope.$broadcast('results', results);
+               };
+               serverHub.client.cardReceived = function() {
+                  $rootScope.$broadcast('cardReceived');
+               };
+
+               $.connection.hub.start({ jsonp: true }).done(function() {
+                  signalRService = $injector.get('signalRService');
+                  
+               });
          }, 500);
 
-      };
+      }
+   };
 
+$scope.signalRHub = '';
 
-      $scope.validIP = false;
+   $scope.validIP = false;
 
       $scope.$watch('signalRSource', function(){
          $timeout.cancel;
@@ -57,7 +64,8 @@
          if(ipRegex.test($scope.signalRSource)){
             var loadSignalRTimeout = $timeout(function() {
                $scope.validIP = true;
-               setSignalRSource();
+               SignalRSource.Init();
+               $scope.signalRHub = $.connection.serverHub;
             }, 1000);            
          }
       }); 
